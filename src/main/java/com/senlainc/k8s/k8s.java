f7d95a025d71f802a6ff21462bf27829be309aa4
bzz256 <axis365@mail.ru>
2020-11-19 13:11:45 +0300

    commit

---
+++ src/main/java/com/senlainc/k8s/k8s.java
@@ -0,0 +1,13 @@
+package com.senlainc.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class k8s {
+
+    public static void main(String[] args) {
+        SpringApplication.run(k8s.class, args);
+    }
+
+}
